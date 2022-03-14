430852d611aa61d3156e750f846a4b70820b657d
nana.like <nana.like@kakaoenterprise.com>
2022-03-14 13:59:47 +0900

    Task 컴포넌트 생성

---
+++ src/components/Task.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Task from './Task';
+
+export default {
+  title: 'Components/Task',
+  component: Task
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2021, 0, 1, 9, 0)
+  }
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED'
+  }
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED'
+  }
+};
