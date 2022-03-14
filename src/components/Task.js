eb69a39ac374a773c86feb14f5e38e2ff9d814f7
nana.like <nana.like@kakaoenterprise.com>
2022-03-14 14:05:22 +0900

    태스크 컴포넌트 UI 고도화

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Task({
   task: { id, title, state },
@@ -6,8 +7,50 @@ export default function Task({
   onPinTask
 }) {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+      <div className="actions" onClick={(e) => e.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          <button onClick={() => onPinTask(id)}>
+            <span
+              className={`icon-star`}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func
+};
