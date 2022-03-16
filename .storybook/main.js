746b9bdcbdea7adc4a0693e0299c17a44cf90c87
nana.like <nana.like@kakaoenterprise.com>
2022-03-16 17:41:56 +0900

    sass-loader 테스트

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,30 +13,29 @@ module.exports = {
     builder: 'webpack5'
   },
   webpackFinal: async (config, { configType }) => {
-    config.module.rules.map((rule) => {
-      if (rule.oneOf) {
-        rule.oneOf = rule.oneOf.slice().map((subRule) => {
-          if (subRule.test instanceof RegExp && subRule.test.test('.scss')) {
-            return {
-              ...subRule,
-              use: [
-                ...subRule.use,
-                {
-                  loader: require.resolve('sass-resources-loader'),
-                  options: {
-                    resources: [
-                      path.resolve(__dirname, '../src/styles/_variable.scss')
-                    ]
-                  }
-                }
-              ]
-            };
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'sass-loader',
+          options: {
+            additionalData: `
+						html {
+              color: red;
+            }
+					`
           }
-          return subRule;
-        });
-      }
-      return rule;
+        }
+      ]
+      // include: path.resolve(__dirname, '../')
     });
+
+    // Return the altered config
     return config;
   }
 };
